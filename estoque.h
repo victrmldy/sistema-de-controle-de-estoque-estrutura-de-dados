c2e41a5a39f119b4099b717805c2c54f8950b393
Lucas Clameirich <lucasclameirich@gmail.com>
2026-09-19 21:13:20 -0400

    Adiciona funcionalidade para exibir total com desconto à vista e implementa a função de aplicar desconto

--- estoque.h
+++ estoque.h
@@ -3,6 +3,7 @@
 
 #define MAX_ITENS 10
 #define TAXA_PADRAO 0.05
+#define TAXA_DESCONTO 0.05
 
 typedef struct {
     int id;
@@ -14,5 +15,6 @@ typedef struct {
 void exibir_menu(void);
 void listar_produtos(Produto lista[], int total);
 float calcular_total(Produto lista[], int total);
+float aplicar_desconto(float total);
 
 #endif
--- main.c
+++ main.c
@@ -6,6 +6,7 @@ void exibir_menu(void) {
     printf("\n=== CONTROLE DE ESTOQUE ===\n");
     printf("1 - Listar produtos\n");
     printf("2 - Exibir valor total em estoque\n");
+    printf("3 - Exibir total com desconto a vista\n");
     printf("0 - Sair\n");
     printf("Escolha uma opcao: ");
 }
@@ -55,6 +56,9 @@ int main(void) {
             case 2:
                 printf("\nTotal em estoque: R$ %.2f\n", calcular_total(estoque, total_produtos));
                 break;
+            case 3:
+                printf("\nTotal com desconto a vista: R$ %.2f\n", aplicar_desconto(calcular_total(estoque, total_produtos)));
+                break;
             case 0:
                 printf("\nEncerrando o programa...\n");
                 break;
@@ -66,3 +70,8 @@ int main(void) {
 
     return 0;
 }
+
+float aplicar_desconto(float total) {
+    return total * (1 - TAXA_DESCONTO);
+}
+
